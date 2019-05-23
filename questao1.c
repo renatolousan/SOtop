d24c804d936da914df84cf2d7286e11bb8eb6251
Renato Lousan da Silva <renatolousan@hotmail.com>
2019-05-23 16:35:54 -0400

    Incremento para struct pedido no trab

--- questao1.c
+++ questao1.c
@@ -2,58 +2,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <math.h>
 
-// Programa simula o deposito e consulta de saldo concorrente de uma conta com 100 reais inicialmente
-// A funçao de depósito deposita 15 reais!!
-// a saída "esperada" é o valor final de 115 reais
+typedef struct cont {
+   int saldo;
+   char nome[64];
+} Conta;
 
-int saldo;
+Conta contaGlobal;
+int valorDeposito;
+char nomeConta[64];
 
-void *deposito(void *t) // deposito de 15 reais na conta
+void *atualiza(void *t)
 {  long tid;
    int s;
+   char n[64];
    tid = (long)t;
-   printf("Thread %ld de deposito lendo o saldo…\n",tid);
+   printf("Thread %ld de atualizacao lendo o saldo…\n", tid);
    sleep(2);
-   s = saldo;
-   printf("Thread %ld de deposito leu o saldo = %d\n",tid, s);
-   s += 15;
-   printf("Thread %ld de deposito escrevendo o saldo = %d\n",tid, s);
+   s = contaGlobal.saldo;
+   printf("Thread %ld de atualizacao leu o saldo = %d\n",tid, s);
+   printf("Thread %ld de atualizacao lendo o nome da conta…\n", tid);
+   sleep(2);
+   strcpy(n, contaGlobal.nome);
+   printf("Thread %ld de atualizacao leu o nome = %s\n",tid, n);
+   s += valorDeposito; 
+   printf("Thread %ld de atualizacao escrevendo o saldo = %d\n",tid, s);
    sleep(3);
-   saldo = s;
-   printf("Thread %ld - valor do saldo = %d\n", tid, saldo);
-   printf("Thread %ld done.\n",tid);
+   contaGlobal.saldo = s;
+   printf("Thread %ld de atualizacao escrevendo o novo nome = %s\n",tid, nomeConta);
+   sleep(3);
+   strcpy(contaGlobal.nome, nomeConta);
+   printf("Thread %ld - valor do saldo = %d\n", tid, contaGlobal.saldo);
+   printf("Thread %ld - nome da conta = %s\n", tid, contaGlobal.nome);
+   printf("Thread %ld terminou.\n",tid);
 }
 
- void *leSaldo(void *t){
+ void *leConta(void *t){
 	long tid;
 	int s;
+   char n[64];
 	tid = (long) t;
 	printf("Thread %ld de consulta lendo o saldo…\n",tid);
 	sleep(2);
-	s = saldo;
+	s = contaGlobal.saldo;
 	printf("Thread %ld de consulta leu o saldo = %d\n",tid, s);
 	sleep(3);
-	//saldo = s;
-	printf("Thread %ld - valor do saldo = %d\n", tid, saldo);
+	// contaGlobal.saldo = s;
+	printf("Thread %ld - valor do saldo = %d\n", tid, contaGlobal.saldo);
+   printf("Thread %ld de consulta lendo o nome da conta…\n",tid);
+	sleep(2);
+	strcpy(n, contaGlobal.nome);
+	printf("Thread %ld de consulta leu o nome = %s\n",tid, n);
+	sleep(3);
+	// strcpy(contaGlobal.nome, n);
+	printf("Thread %ld - nome da conta = %s\n", tid, contaGlobal.nome);
 	printf("Thread %ld terminou.\n",tid);
 
 } 
 
 int main(int argc, char *argv[])
 {
-   int threadsLeitoras;
-   int threadsEscritoras;
+   int threadsLeitoras, threadsEscritoras;
+
    printf("Digite a quantidade de threads leitoras: \n");
    scanf("%d", &threadsLeitoras);
    printf("Digite a quantidade de threads escritoras: \n");
    scanf("%d", &threadsEscritoras);
+   printf("Digite o saldo inicial da conta: \n");
+   scanf("%d", &contaGlobal.saldo);
+   printf("Digite o valor que deseja depositar (use numeros negativos para simular saque) \n");
+   scanf("%d", &valorDeposito);
+   printf("Digite um nome para conta \n");
+   scanf("%s", contaGlobal.nome);
+   printf("Digite um nome para qual deseja atualizar \n");
+   scanf("%s", nomeConta);
+
    pthread_t thread[threadsLeitoras + threadsEscritoras]; // vetor de threads
    pthread_attr_t attr;
-
-   saldo=100;
 
    /* Initialize and set thread detached attribute */
    pthread_attr_init(&attr);
@@ -61,18 +89,19 @@ int main(int argc, char *argv[])
 
    for(int i = 0; i<threadsEscritoras; i++) {
       printf("Main: criando thread escritora (deposito) %d\n", i);
-	  	  pthread_create(&thread[i], &attr, deposito, (void *)i);
+	  	  pthread_create(&thread[i], &attr, atualiza, (void *)i);
    }
 
       for(int i = threadsEscritoras; i<threadsEscritoras+threadsLeitoras; i++) {
       printf("Main: criando thread leitora %d\n", i);
-	  pthread_create(&thread[i], &attr, leSaldo, (void *)i);
+	  pthread_create(&thread[i], &attr, leConta, (void *)i);
    }
    pthread_attr_destroy(&attr);
 
    sleep(15); // main dorme bastante para acordar após as thread terem terminado e pegar o saldo final 
 
-   printf("Valor final do saldo = %d\n", saldo);
+   printf("Valor final do saldo = %d\n", contaGlobal.saldo);
+   printf("Valor final do nome = %s\n", contaGlobal.nome);
    printf("Main: program completed. Exiting.\n");
    pthread_exit(NULL);
    return(0);
